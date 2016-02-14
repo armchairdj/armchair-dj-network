c99aec6ba4800efeae661c094855202325863bd3
Brian J. Dillard <armchairdj@gmail.com>
2016-02-13 16:21:01 -0800

    - Update Gulp components and install them. - Get gulpfile working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
+/**
+ * External dependencies.
+ */
+
 var gulp           = require('gulp');
 
-var axis           = require('axis-css');
+var _              = require('underscore');
+var axis           = require('axis');
 var concat         = require('gulp-concat');
-var cssmin         = require('gulp-minify-css');
+var cssnano        = require('gulp-cssnano');
 var es             = require('event-stream');
 var events         = require('events');
 var nib            = require('nib');
@@ -14,83 +19,89 @@ var streamqueue    = require('streamqueue');
 var stylus         = require('gulp-stylus');
 var through        = require('through2');
 var uglify         = require('gulp-uglify');
-var _              = require('underscore');
+
+/**
+ * Internal dependencies.
+ */
+
+var config          = require('./lib/util/configure')();
 
 /*
   The following line fixes this problem:
     (node) warning: possible EventEmitter memory leak detected. 11 listeners added.
     Use emitter.setMaxListeners() to increase limit.
 */
+
 events.EventEmitter.prototype._maxListeners = 100;
 
-/***** File locations *****/
+/*
+  Source files.
+*/
 
 var assetLocations = {
-  dev: 'public/assets/d',
-  pro: 'public/assets/p'
+  development: './public/asset/d',
+  production:  './public/asset/p'
 };
 
-/*
-  Structured hash of source files.
-  Hash structure determines which tasks will be created.
-  As you add components here, add a task to the 'tasks' array below.
-*/
 var sourceFiles = {
   styles: {
     site: {
       css:  [],
-      styl: []
+      styl: [
+        './lib/asset/css/site/site.styl'
+      ]
     },
   },
 
   scripts: {
     modernizr: {
       js: [
-        'lib/assets/js/vendor/modernizr.js',
+        './lib/asset/js/vendor/modernizr.js'
       ]
     },
 
     site: {
       js: [
-        'lib/assets/js/vendor/jquery/jquery-2.0.2.js',
+        './lib/asset/js/vendor/jquery-2.0.2.js'
       ]
     }
   }
 };
 
-/***** Helpers: Source hash lookup *****/
+/**
+ * Functions: General.
+ */
+
 
 function arrayicize() {
   return _.chain(arguments).toArray().flatten().compact().value();
 }
 
-/* Pluck a raw array of files from the tree - or null */
 function fileList(type, component, format) {
   var list = arrayicize( sourceFiles[type][component][format] );
 
   return list && list.length ? list : null;
 }
 
-/* Pluck a gul.src list of files from the tree - or null */
 function srcList(type, component, format) {
   var list = fileList(type, component, format);
 
   return list ? gulp.src(list) : null;
 }
 
-/***** CSS concatenation & compilation *****/
+/**
+ * Functions: CSS.
+ */
 
-/* Build dev and prod version of a CSS component from .css and .styl files */
 function concatStyles(component) {
   return compileStyles(component)
     .pipe(concat(component + '.css'))
-    .pipe(gulp.dest(assetLocations.dev))
-    .pipe(cssmin())
-    .pipe(gulp.dest(assetLocations.pro))
+    .pipe(gulp.dest(assetLocations.development))
+    .pipe(cssnano())
+    .pipe(gulp.dest(assetLocations.production))
   ;
 }
 
-/* Prepare a CSS component for concatenation and minification */
 function compileStyles(component) {
   var stream = streamqueue({ objectMode: true });
   var css    = srcList('styles', component, 'css' );
@@ -107,8 +118,6 @@ function compileStyles(component) {
   return stream.done();
 }
 
-/***** Stylus compilation *****/
-
 function compileStylus(src) {
   return src
     .pipe(plumber())
@@ -120,21 +129,21 @@ function compileStylus(src) {
   ;
 }
 
-/***** JS concatenation & compilation *****/
+/**
+ * Functions: JS.
+ */
 
-/* Build dev and prod version of a JS component from .js files */
 function concatScripts(component) {
   return compileScripts(component)
     .pipe(concat(component + '.js'))
-    .pipe(gulp.dest(assetLocations.dev))
+    .pipe(gulp.dest(assetLocations.development))
     .pipe(uglify({
       mangle: false
     }))
-    .pipe(gulp.dest(assetLocations.pro))
+    .pipe(gulp.dest(assetLocations.production))
   ;
 }
 
-/* Prepare a JS component for concatenation and minification */
 function compileScripts(component) {
   var stream = streamqueue({ objectMode: true });
 
@@ -147,9 +156,10 @@ function compileScripts(component) {
   return stream.done();
 }
 
-/***** Helpers for creating default and watch tasks *****/
+/**
+ * Functions: Gulp.
+ */
 
-/* Create a task to create a JS or CSS component via our sourceFiles naming conventions */
 function createTask(taskName) {
   var type      = taskName.split('-')[0];
   var component = taskName.split('-')[1];
@@ -168,7 +178,6 @@ function createTask(taskName) {
   });
 }
 
-/* Create a watcher via our sourceFiles naming conventions */
 function createWatcher(taskName) {
   var type      = taskName.split('-')[0];
   var component = taskName.split('-')[1];
