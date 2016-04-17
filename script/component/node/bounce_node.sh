2f2c0c286c986a50dd96d08a5c0d4508f501b4e5
Brian J. Dillard <armchairdj@gmail.com>
2016-04-16 18:41:49 -0700

    - Fix file path.

--- script/component/node/bounce_node.sh
+++ script/component/node/bounce_node.sh
@@ -5,10 +5,8 @@ FILE_PATH="$APP_ROOT/script/component/node/file"
 
 echo "$SCRIPT_NAME: BEGIN"
 
-cd "$APP_ROOT/script/component/node"
-
 echo "$SCRIPT_NAME: update config"
-sudo cp "./node.$APP_ENV.conf" /etc/init/node.conf
+sudo cp "$FILE_PATH/node.$APP_ENV.conf" /etc/init/node.conf
 
 echo "$SCRIPT_NAME: bounce"
 sudo stop node
